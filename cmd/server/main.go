a27e2ec79f75401a8cdfebcac15c9534791d4915
rafaeldiazmiles <rafaeldiazmiles@gmail.com>
2021-12-27 17:13:46 -0300

    Added health check endpoint

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	transportHTTP "github.com/rafaeldiazmiles/API-rest-tutorialedge/internal/http"
+)
+
+// App - the struct which contains things like pointers
+// to database connections
+type App struct{}
+
+//Run - sets up our application
+func (app *App) Run() error {
+	fmt.Println("Setting up our App")
+
+	handler := transportHTTP.NewHandler()
+	handler.SetupRoutes()
+
+	if err := http.ListenAndServe(":8080", handler.Router); err != nil {
+		fmt.Println("Failed to setup server")
+		return err
+	}
+
+	return nil
+}
 
 func main() {
 	fmt.Println("Go REST API Course")
+	app := App{}
+	if err := app.Run(); err != nil {
+		fmt.Println("Error starting up our REST API")
+		fmt.Println(err)
+	}
 }
